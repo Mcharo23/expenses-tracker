c0ec165fbd71dcd4f211e43aa7ef1fa454a93b63
Mcharo23 <mcharoprofg23@gmail.com>
2024-04-11 19:19:29 +0300

    transaction repository added

--- src/main/java/com/pairlearning/expensetracker/category/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pairlearning.expensetracker.category.repository;
-
-import com.pairlearning.expensetracker.category.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
