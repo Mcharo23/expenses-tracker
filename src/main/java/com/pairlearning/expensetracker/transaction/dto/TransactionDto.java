d8c441943efef052f71a686235ac5f817a4ca2ec
Mcharo23 <mcharoprofg23@gmail.com>
2024-04-11 19:31:43 +0300

    fixing bug

---
+++ src/main/java/com/pairlearning/expensetracker/transaction/dto/TransactionDto.java
@@ -0,0 +1,4 @@
+package com.pairlearning.expensetracker.transaction.dto;
+
+public class TransactionDto {
+}
---
+++ src/main/java/com/pairlearning/expensetracker/transaction/service/TransactionService.java
@@ -0,0 +1,4 @@
+package com.pairlearning.expensetracker.transaction.service;
+
+public interface TransactionService {
+}
