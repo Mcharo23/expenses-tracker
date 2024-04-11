b3680dfb7f220fed01c58a7676befbd46ea05788
Mcharo23 <mcharoprofg23@gmail.com>
2024-04-11 19:32:13 +0300

    fixing bug

--- src/main/java/com/pairlearning/expensetracker/transaction/dto/TransactionDto.java
+++ src/main/java/com/pairlearning/expensetracker/transaction/dto/TransactionDto.java
@@ -1,4 +1,25 @@
 package com.pairlearning.expensetracker.transaction.dto;
 
+import com.pairlearning.expensetracker.category.entity.Category;
+import com.pairlearning.expensetracker.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class TransactionDto {
+
+    private int transactionId;
+
+    private Category category;
+
+    private User user;
+
+    private Double amount;
+
+    private String note;
+
+    private Long transactionDate;
 }
--- src/main/java/com/pairlearning/expensetracker/transaction/service/TransactionService.java
+++ src/main/java/com/pairlearning/expensetracker/transaction/service/TransactionService.java
@@ -1,4 +1,9 @@
 package com.pairlearning.expensetracker.transaction.service;
 
+import com.pairlearning.expensetracker.transaction.dto.TransactionDto;
+
+import java.util.List;
+
 public interface TransactionService {
+    List<TransactionDto> fetchAllTransaction(Integer userId, Integer categoryId);
 }
